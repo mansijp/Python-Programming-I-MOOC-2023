5dc8030701b8a8153d8370f34f88eaf7b4f991ad
Mansi Patel <45537538+mansijp@users.noreply.github.com>
2023-04-28 18:45:13 -0400

    Updating solutions for questions 17-20, part 3

---
+++ part03/17 - substrings_1.py
@@ -0,0 +1,6 @@
+string = input("Please type in a string: ")
+i = 1
+
+while  i <= len(string):
+    print(string[:i])
+    i += 1
---
+++ part03/20 - find_the_first_substring.py
@@ -0,0 +1,3 @@
+word = input("Please type in a word: ")
+char = input("Please type in a character: ")
+
