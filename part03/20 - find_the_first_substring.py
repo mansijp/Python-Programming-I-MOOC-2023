69ffee5f9bf60ea1f71799bc910fbfec9aad6616
Patel <Mansi.Patel1@ontario.ca>
2023-09-12 09:47:26 -0400

    Q 20 Part 3 Solution

--- part03/20 - find_the_first_substring.py
+++ part03/20 - find_the_first_substring.py
@@ -1,3 +1,17 @@
 word = input("Please type in a word: ")
 char = input("Please type in a character: ")
+output = ""
 
+for i in range(len(word)):
+    j = word.find(char)
+    if j!=-1 and (j+2) < len(word):
+        for k in range(3):
+            output += word[j+k]
+        break
+print( output )
+
+## Model solution ##
+
+# index = word.find(character)
+# if index!=-1 and len(word)>=index+3:
+#     print(word[index:index+3])
