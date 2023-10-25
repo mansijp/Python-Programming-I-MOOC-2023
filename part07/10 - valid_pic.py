f34cdea4b25e8563e057c24062b66aeaeb57637b
Patel <mansijpatel321@gmail.com>
2023-10-25 16:26:27 -0400

    Solutions 9 and 10 for part 7

---
+++ part07/10 - valid_pic.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+day = int(input("Day: "))
+month = int(input("Month: "))
+year = int(input("Year: "))
+
+birthday = datetime(year, month, day)
+millenium = datetime(1999, 12, 31)
+difference = millenium - birthday
+
+if difference.days > 0:
+    print(f"You were {difference.days} days old on the eve of the new millennium.")
+else:
+    print("You weren't born yet on the eve of the new millennium.")
